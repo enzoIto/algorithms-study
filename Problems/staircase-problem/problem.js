37788198bde0e2c8862a1ae10db28b2038500046
Massaki <massakivx@gmail.com>
2023-01-08 15:41:17 -0300

    implementing generalized case for climbing staircase problem

---
+++ Problems/staircase-problem/problem.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  const ways = new Array(n).fill(0);
+  ways[0] = 1
+  ways[1] = 1
+  for (let i = 2; i <= n; i++) {
+    ways[i] = ways[i-1] + ways[i-2];
+  }
+  return ways[n]
+}
